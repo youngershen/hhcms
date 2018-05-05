c0c8e451db7dc578007154aec3a8fd4ac3930ca3
younger shen <younger.shen@hotmail.com>
2018-05-05 18:58:42 +0800

    add handlebars

--- themes/default/js/default.js
+++ themes/default/js/default.js
@@ -1,21 +1,21 @@
 "use strict";
 
-$(function(){
+$(function () {
     const $ = window.$;
-    $.meta = function(name){
+    const handlerbars = window.Handlebars;
+    $.meta = function (name) {
         return $("meta[name=" + name + "]").attr('content');
     };
 
     let hhcms = {};
+
     window.hhcms = hhcms;
 
-    hhcms.csrf_token = $("meta[name=CSRF_TOKEN]").attr('content');
+    hhcms.csrf_token = $.meta('CSRF_TOKEN');
 
-    // init the whole page popover
     $('[data-toggle="popover"]').popover();
 
-    hhcms.user_exists = function(username, email, success_callback, error_callback)
-    {
+    hhcms.user_exists = function (username, email, success_callback, error_callback) {
         let payload = {
             'username': username,
             'email': email
@@ -33,5 +33,29 @@ $(function(){
         });
 
     };
+
+
+    $('#username').blur(function () {
+        $('.warning').remove();
+
+        let username = $('#username').val();
+        let source = document.getElementById("username-template").innerHTML;
+        let template = handlerbars.compile(source);
+
+        if (!username) {
+            let context = {message: "username is required"};
+            let html = template(context);
+            $(html).insertAfter('#username-row')
+        }
+        else {
+            hhcms.user_exists(username, null, function (data) {
+                if (data.status) {
+                    let context = {message: "username already exists"};
+                    let html = template(context);
+                    $(html).insertAfter('#username-row')
+                }
+            });
+        }
+    });
 });
 
