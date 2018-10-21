a14bd323f0dd9ff2054ee0a4eb52beebb8f98615
Younger Shen <shenyangang@163.com>
2018-10-21 16:44:46 +0800

    update user db model

--- hhcms/apps/account/models.py
+++ hhcms/apps/account/models.py
@@ -1,46 +1,67 @@
 from django.utils.translation import ugettext as _
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from hhcms.apps.common.models import Model
 
 # Create your models here.
 
 
+class Manager(UserManager):
+    pass
+
+
 class User(AbstractUser, Model):
-    username = models.CharField(max_length=150,
+    username = models.CharField(max_length=100,
                                 unique=True,
-                                verbose_name=_('username'))
+                                verbose_name=_('Username'))
 
-    email = models.EmailField(db_index=True,
-                              blank=True,
+    email = models.EmailField(unique=True,
                               null=True,
-                              default='',
-                              verbose_name=_('email address'),)
+                              verbose_name=_('Email'), )
 
-    cellphone = models.CharField(db_index=True,
-                                 max_length=255,
-                                 blank=True,
+    cellphone = models.CharField(max_length=100,
+                                 unique=True,
                                  null=True,
-                                 default='',
-                                 verbose_name=_('cellphone number'),)
+                                 verbose_name=_('Cellphone'), )
 
-    EMAIL_FIELD = 'email'
-    CELLPHONE_FIELD = 'cellphone'
+    property = models.
+
+
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
 
-    def text_user(self, message):
+    objects = Manager()
+
+    def get_username_field(self):
+        return self.USERNAME_FIELD
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
         pass
 
-    @classmethod
-    def get_cellphone_field_name(cls):
-        try:
-            return cls.CELLPHONE_FIELD
-        except AttributeError:
-            return 'email'
-
-    def get_cellphone(self):
-        return getattr(self, self.get_cellphone_field_name(), None)
+    def text_user(self):
+        pass
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+
+class InfoRecord(Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='info_records',
+                             related_query_name='info')
+
+    name = models.CharField(max_length=255,
+                            db_index=True,
+                            verbose_name=_('Name'))
+
+    value = models.CharField(max_length=255,
+                             blank=True,
+                             null=True,
+                             verbose_name=_('Value'))
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = _('Info Record')
+        verbose_name_plural = _('Info Records')
